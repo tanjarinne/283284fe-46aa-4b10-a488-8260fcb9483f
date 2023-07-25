1bd9d9ba1e64c44263405d8299d2037e32f0d208
Tanja Alva <tanja.alva@gmail.com>
2023-07-26 00:11:07 +0800

    Add non-persistent function to receive a request, shorten it, and generate a response

--- app/src/main.rs
+++ app/src/main.rs
@@ -6,16 +6,18 @@ use rocket::http::Status;
 extern crate rocket;
 
 mod structures;
-use structures::HealthResponse;
+use structures::{HealthResponse, UrlRequest, Response};
 
 mod db;
+
 mod shortener;
+use shortener::generate_shortened_hash;
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .manage(db::mount_aerospike_client())
-        .mount("/", routes![healthcheck])
+        .mount("/", routes![healthcheck, handle_new_url])
 }
 
 #[get("/health")]
@@ -23,6 +25,15 @@ pub async fn healthcheck(state: &State<db::AppState>) -> Result<Json<HealthRespo
     let response = HealthResponse {
         status: state.aerospike.is_connected().to_string(),
         message: "dummy message".to_string(),
+    };
+    Ok(Json(response))
+}
+
+#[post("/url", data="<body>")]
+pub async fn handle_new_url(state: &State<db::AppState>, body: Json<UrlRequest<'_>>) -> Result<Json<Response>, Status> {
+    let response = Response {
+        shorten_url: generate_shortened_hash(&body.url.to_string()),
+        url        : body.url.to_string(),
     };
     Ok(Json(response))
 }
--- app/src/structures.rs
+++ app/src/structures.rs
@@ -1,4 +1,15 @@
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
+
+#[derive(Deserialize)]
+pub struct UrlRequest<'r> {
+  pub url: &'r str,
+}
+
+#[derive(Serialize)]
+pub struct Response {
+  pub url        : String,
+  pub shorten_url: String
+}
 
 #[derive(Serialize)]
 pub struct HealthResponse {
